5abfa67a6f9c0a914ded12796b5643f066780561
hta <agathamagnusg@hotmail.com>
2021-09-22 13:05:55 -0300

    Sorteando-Ordem-Na-Lista

---
+++ SorteandoOrdemNaLista/index.py
@@ -0,0 +1,9 @@
+#Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+n1 = str(input('Primeira opção: '))
+n2 = str(input('Segunda opção: '))
+n3 = str(input('Terceira opção: '))
+n4 = str(input('Quarto opção: '))
+lista = [n1, n2, n3, n4]
+random.shuffle(lista)
+print('A ordem de apresentação será')
+print(lista)
