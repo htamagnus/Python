8402010cddae45feb77405b10f0c2be72e6b01f4
hta <agathamagnusg@hotmail.com>
2021-09-29 11:42:28 -0300

    Antecessor-e-Sucessor

---
+++ Antecessor-e-Sucessor/index.py
@@ -0,0 +1,6 @@
+#DESAFIO 005: ANTECESSOR E SUCESSOR
+#Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.
+n= int(input('Digite um número: '))
+a = n - 1
+s = n + 1
+print('Analisando o valor {}, seu antecessor é {} e o sucessor é {}'.format(n,(n-1),(n+1)))
---
+++ Antecessor-e-Sucessor/readme.md
@@ -0,0 +1,1 @@
+Programa que lê um número Inteiro e mostra na tela o seu sucessor e seu antecessor.
