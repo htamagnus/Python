0d7a442ad463a60b73f5c3c7b98c3da3a2dbf8d7
hta <agathamagnusg@hotmail.com>
2021-10-02 16:30:19 -0300

    Conversor-de-Medidas

---
+++ Conversor-de-Medidas/index.py
@@ -0,0 +1,6 @@
+#DESAFIO 008: CONVERSOR DE MEDIDAS
+#Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
+medida = float(input('Uma distância em metros: '))
+cm = medida * 100
+mm = medida * 1000
+print('A medida de {}m corresponde a {}cm e {}mm'.format(medida, cm, mm))
