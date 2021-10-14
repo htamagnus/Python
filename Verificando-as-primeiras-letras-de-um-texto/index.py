7d87f80e3dcec0ec48604b7838c8140a9904c9fa
hta <agathamagnusg@hotmail.com>
2021-10-14 13:40:32 -0300

    Verificando-as-primeiras-letras-de-um-texto

---
+++ Verificando-as-primeiras-letras-de-um-texto/index.py
@@ -0,0 +1,4 @@
+#DESAFIO 24: VERIFICANDO AS PRIMEIRAS LETRAS DE UM TEXTO
+#Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome “SANTO”.
+cid = str(input('Em que cidade você nasceu? ')).strip()
+print(cid[:5].upper() == 'Santo')
---
+++ Verificando-as-primeiras-letras-de-um-texto/readme.md
@@ -0,0 +1,1 @@
+Programa que lê o nome de uma cidade diga se ela começa ou não com o nome “SANTO”.
