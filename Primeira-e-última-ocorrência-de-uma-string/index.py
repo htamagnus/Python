4a8f93195ec728670adead8a27d00672015b64e3
hta <agathamagnusg@hotmail.com>
2021-10-15 10:29:03 -0300

    Primeira-e-última-ocorrência-de-uma-String

---
+++ Primeira-e-última-ocorrência-de-uma-string/index.py
@@ -0,0 +1,14 @@
+#DESAFIO 26: Primeira e última ocorrência de uma string
+#Faça um programa que leia uma frase pelo teclado e mostre quantas vezes aparece a letra “A”, em que posição ela aparece a primeira vez e em que posição ela aparece a última vez.
+frase = str(input('Digite uma frase: ')).upper().strip()
+print('A letra A aparece {} vezes na frase.'.format(frase.count('A')))
+print('A primeira letra A apareceu na posição {}'.format(frase.find('A')+1))
+print('A última letra A apareceu na posição {}'.format(frase.rfind('A')+1))
+
+#DESAFIO 27
+#Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
+n = str(input('Digite seu nome completo: ')).strip()
+nome = n.split()
+print('Muito prazer em te conhecer!')
+print('Seu primeiro nome é {}'.format(nome[0]))
+print('Seu último nome é {}'.format(nome[len(nome)-1]))
---
+++ Primeira-e-última-ocorrência-de-uma-string/readme.md
@@ -0,0 +1,3 @@
+Programa que lê uma frase pelo teclado e mostre quantas vezes aparece a letra “A”, em que posição ela aparece a primeira vez e em que posição ela aparece a última vez.
+
+E, um programa que lê o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
